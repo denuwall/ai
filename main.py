c42abef3ee6182f821daad9b92c5a51a0e209240
denuwall <dkcannal@gmail.com>
2023-09-09 00:09:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#from docx import Document
+from docx import Document
 
 # Функция для анализа текста (заглушка)
 def analyze_text_with_nn(text):
@@ -12,7 +12,7 @@ def analyze_text_with_nn(text):
 def upload_file():
     uploaded_file = st.file_uploader("Загрузите файл Word (.docx)", type=["docx"])
     if uploaded_file is not None:
-        #doc = Document(uploaded_file)
+        doc = Document(uploaded_file)
         full_text = "\n".join([p.text for p in doc.paragraphs])
         return full_text
     return None
@@ -56,11 +56,10 @@ def main():
             if text:
                 category, highlighted_text = analyze_text_with_nn(text)
 
-                # Отображение результата в модальном окне
-                with st.modal():
-                    st.header("Результат анализа:")
-                    st.markdown(f"**Категория:** {category}")
-                    st.markdown(f"**Выделенные элементы:** {highlighted_text}", unsafe_allow_html=True)
+                # Отображение результата
+                st.subheader("Результат анализа:")
+                st.markdown(f"**Категория:** {category}")
+                st.markdown(f"**Выделенные элементы:** {highlighted_text}", unsafe_allow_html=True)
             else:
                 st.warning("Загрузите текст для анализа")
 
